fc8b3a4304b4996807579fa7a58a6d5a33515bdf
yangjiyeon <970619abc@naver.com>
2019-07-29 16:38:35 -0700

    implement the drawGraph function based on whether a node is exist or not

--- graph-viz/app/controllers/GraphController.java
+++ graph-viz/app/controllers/GraphController.java
@@ -1,56 +1,29 @@
 package controllers;
 
+import java.io.*;
+import java.math.BigInteger;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.net.InetSocketAddress;
+
+
+import play.libs.Json;
+
+import com.sun.net.httpserver.HttpContext;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.core.env.PropertyResolver;
 import play.mvc.*;
-import play.twirl.api.Html;
-import views.html.*;
-
-import java.net.*;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-
-import play.libs.Json;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
 
 
-
-/*
-import org.elasticsearch.action.delete.DeleteResponse;
-import org.elasticsearch.action.get.GetResponse;
-import org.elasticsearch.action.index.IndexRequest;
-import org.elasticsearch.action.index.IndexResponse;
-import org.elasticsearch.action.update.UpdateRequest;
-import org.elasticsearch.client.transport.TransportClient;
-import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.transport.client.PreBuiltTransportClient;
-
-*/
-
-
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-import java.util.Map;
-
-
-/**
- * This controller contains an action to handle HTTP requests
- * to the application's home page.
- */
 
 public class GraphController extends Controller {
 
@@ -58,126 +31,114 @@ public class GraphController extends Controller {
     @Autowired
     private PropertyResolver propertyResolver;
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
+    private static final String POST_URL = "http://128.195.10.93:8080/";
+
 
     public Result index(String query) throws IOException {
 
-        //URL url = new URL("http://localhost:9200/wiki/test/" + query + "?pretty");
+        List<String> title_list = new ArrayList<String>();
 
-        //dataset.json -> test.json (동준오빠가 만든거)
+        URL obj = new URL(POST_URL);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
 
-        URL url = new URL("http://localhost:9200/wiki/test/_search?q=text:" + query);
-        //URL url = new URL("http://128.195.10.93:8080?keyword=" + query);
+        //add reuqest header
+        con.setRequestMethod("POST");
 
+        String urlParameters = "{\"keyword\" : \"" + query + "\"}";
 
-        //data.json (혜림언니가 만든거)
-        //dataset.json (500메)
-        //URL url = new URL("http://localhost:9200/test/page/_search?q=text:" + query);
+        // Send post request
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
 
-        HttpURLConnection con = (HttpURLConnection) url.openConnection();
-        con.setRequestMethod("GET");
+        int responseCode = con.getResponseCode();
+        System.out.println("\nSending 'POST' request to URL : " + obj);
+        System.out.println("Post parameters : " + urlParameters);
+        System.out.println("Response Code : " + responseCode);
 
-        // 문자열로 URL 표현
-        System.out.println("URL :" + url.toExternalForm());
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        StringBuffer response = new StringBuffer();
 
-        // HTTP Connection 구하기
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-
-        // 요청 방식 설정 ( GET or POST or .. 별도로 설정하지않으면 GET 방식 )
-        conn.setRequestMethod("GET");
-
-        // 연결 타임아웃 설정
-        conn.setConnectTimeout(3000); // 3초
-        // 읽기 타임아웃 설정
-        conn.setReadTimeout(3000); // 3초
-
-        // 요청 방식 구하기
-        System.out.println("getRequestMethod():" + conn.getRequestMethod());
-        // 응답 콘텐츠 유형 구하기
-        System.out.println("getContentType():" + conn.getContentType());
-        // 응답 코드 구하기
-        System.out.println("getResponseCode():"    + conn.getResponseCode());
-        // 응답 메시지 구하기
-        System.out.println("getResponseMessage():" + conn.getResponseMessage());
-
-        // 응답 헤더의 정보를 모두 출력
-        for (Map.Entry<String, List<String>> header : conn.getHeaderFields().entrySet()) {
-            for (String value : header.getValue()) {
-                System.out.println(header.getKey() + " : " + value);
-            }
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
         }
+        in.close();
 
-        System.out.println("hello");
+        //응답의 140번째 인덱스부터 hits를 찾음 (그러면 첫번째 hits를 지나서 찾기 시작)
+        String hits_str = response.substring(response.indexOf("hits",140), response.length()-2);
 
+        //핵심 hits 안에 raw data들이 있는데, 기사의 구분자인 }, 로 나눔
+        String hits_list[] = hits_str.split("},");
 
+        int cnt_tmp = 0;
+        ObjectNode result = Json.newObject();
+        String text = "";
 
-        // 응답 내용(BODY) 구하기
-        try (InputStream in = conn.getInputStream();
-             ByteArrayOutputStream out = new ByteArrayOutputStream()) {
-
-            ObjectNode result = Json.newObject();
-
-            byte[] buf = new byte[1024 * 8];
-            int length = 0;
-            while ((length = in.read(buf)) != -1) { //buf로 읽는 InputStream의 내용이 없을때까지 out에 담는다.
-                out.write(buf, 0, length);
-            }
-            result.put("query", query); //브라우저에 내용 출력하기
-            result.put("body", out.toString()); //내용이 담긴 out을 json객체인 result에 담는다.
-
-            // 접속 해제
-            conn.disconnect();
-            return ok(result);
-        }
-
-
-
+        String to_list = "";
         /*
-
-        ArrayNode replies = Json.newArray();
-        try {
-            Class.forName("org.postgresql.Driver"); //환경변수를 포함하는 JAR 파일들 중 이 클래스를 찾는
-            Connection conn = DriverManager
-                    .getConnection("jdbc:postgresql://localhost:5432/graphtweet",
-                            "graphuser", "graphuser");  //인자인 DB URL을 가지고 DB에 연결
-                         //이건 다른 웹서버의 엘라스틱에 연결하는거고, 로컬에 엘라스틱 깔면 더 빠르다.
-
-            Statement statement = conn.createStatement(); //DB에 보낼 SQL statement object를 만
-            ResultSet resultSet = statement.executeQuery("select " +
-                    "from_longitude, from_latitude, " +
-                    "to_longitude, to_latitude " +
-                    "from replytweets where " +
-                    "to_tsvector('english', from_text) " +
-                    "@@ to_tsquery('"+query+"') or " +
-                    "to_tsvector('english', to_text) " +
-                    "@@ to_tsquery('"+query+"');");
-            //주어진 SQL문을 실행하고, ResultSet 객체를 반환
-
-            while (resultSet.next()) { //SQL결과인 ResultSet의 다음줄로 이동
-                ObjectNode reply = Json.newObject();
-                ArrayNode fromCoordinate = Json.newArray();
-                fromCoordinate.add(resultSet.getFloat("from_longitude"));
-                fromCoordinate.add(resultSet.getFloat("from_latitude"));
-                reply.set("source", fromCoordinate);
-                ArrayNode toCoordinate = Json.newArray();
-                toCoordinate.add(resultSet.getFloat("to_longitude"));
-                toCoordinate.add(resultSet.getFloat("to_latitude"));
-                reply.set("target", toCoordinate);
-                replies.add(reply);
-            }
-            resultSet.close();
-            statement.close();
-            conn.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-        return ok(Json.toJson(replies));
-
+           [ [ㅁ,ㅁ], [ㅁ], [ㅁ,ㅁ,ㅁ] ] 가 js에서 ,으로 파싱하기 힘들어서
+             [ㅁ,ㅁ]  [ㅁ]  [ㅁ,ㅁ,ㅁ] 은 똑같이 만드는데
+             [ㅁ,ㅁ],,[ㅁ],,[ㅁ,ㅁ,ㅁ],, 처럼 'list.toString() + ,,'로 스트링으로 저장하면 파싱하기 쉬울 듯
         */
+
+        for(String str : hits_list) {
+            //source
+            //각 str에서 source(title, text)추출
+            String source = str.substring(str.indexOf("source"), str.length() - 2);
+
+            //title
+            //{"ans":"source\": {\"title\": \"Computer terminal\""}
+            String title = source.split(",")[0];
+
+            //"title\": \"제목\""에서 제목만 추출
+            title = title.substring(title.indexOf("title") + 9, title.length() - 1);
+
+            //add the title to the title list
+            title_list.add(title);
+
+            //to_list
+            //각 to_list안의 리스트들
+            List<String> link_list = new ArrayList<String>();
+
+            //source에서 title과 text가 ,으로 구분되어있는데 limit 인자를 안넣으면 text안의 ,들도 쪼개짐
+            //limit는 split으로 만들어지는 리스트의 최대 원소 수
+            String[] results = source.split(",",2);
+            text = results[1];
+
+            //text안에서 [[를 구분자로 split (link들이 [[링크]] 형태이므로)
+            String[] list_tmp = text.split("\\[\\[");
+
+            cnt_tmp = list_tmp.length;
+
+            //find links in a article
+            int flag = 0;
+            for (String ele :list_tmp) {
+                //맨 첫 원소는 쓰레기 값
+                if (flag == 0){
+                    flag =1;
+                    continue;
+                }
+                String ele_tmp = ele;
+                String link = ele_tmp.split("\\]\\]")[0];
+
+                link_list.add(link);
+            }
+
+            to_list = to_list + link_list.toString();
+            to_list = to_list + ",,";
+        }
+
+        result.put("////// 디버깅 //////", cnt_tmp);
+        result.put("title_list", title_list.toString());
+        result.put("to_list", to_list);
+
+        //print result
+        System.out.println(response.toString());
+
+        return ok(result);
     }
 }
